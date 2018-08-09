9fddc78ac4f98d0f5fcdf918c56043e08a5699c8
gc42 <gc3d.42@gmail.com>
2018-08-09 13:34:25 +0200

    Initialisation, tuto termine

---
+++ Assets/Scripts/AdjustScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AdjustScript : MonoBehaviour {
+
+	private void OnGUI()
+	{
+        int rectR = 10;
+        int rectL = 120;
+        int rectH = 30;
+
+
+        if (GUI.Button(new Rect(rectR, 100, rectL, rectH), "Health up"))
+        {
+            GameControl.control.health += 10;
+        }
+        if (GUI.Button(new Rect(rectR, 150, rectL, rectH), "Health down"))
+        {
+            GameControl.control.health -= 10;
+        }
+        if (GUI.Button(new Rect(rectR, 200, rectL, rectH), "Experience up"))
+        {
+            GameControl.control.experience += 10;
+        }
+        if (GUI.Button(new Rect(rectR, 250, rectL, rectH), "Experience down"))
+        {
+            GameControl.control.experience -= 10;
+        }
+        if (GUI.Button(new Rect(rectR, 300, rectL, rectH), "Save"))
+        {
+            GameControl.control.Save();
+        }
+        if (GUI.Button(new Rect(rectR, 350, rectL, rectH), "Load"))
+        {
+            GameControl.control.Load();
+        }
+	}
+}
---
+++ Assets/Scripts/TraverseScreensScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class TraverseScreensScript : MonoBehaviour
+{
+
+    public int sceneToLoad = 0;
+
+    private void OnGUI()
+    {
+        sceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+        if (SceneManager.GetActiveScene().buildIndex + 1 >= SceneManager.sceneCountInBuildSettings)
+        {
+            sceneToLoad = 0;
+        }
+
+        GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height - 80, 100, 30), "Current Scene: " + SceneManager.GetActiveScene().buildIndex);
+        //GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height - 65, 100, 30), "Scene to load: " + sceneToLoad);
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height - 50, 100, 40), "Load Scene " + (sceneToLoad)))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
